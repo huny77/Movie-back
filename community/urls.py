af8f4a965e814b81038e9376c9f22a595e2cd75f
huny <hanjoi77@naver.com>
2021-05-26 09:28:58 +0900

    first commit

---
+++ community/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.post_list_create),
+    path('<int:post_pk>/', views.post_delete_update_detail_comment_create),
+    path('<int:post_pk>/like/', views.like_post),
+    path('<int:post_pk>/<comment_pk>/', views.comment_update_delete),
+]
+
